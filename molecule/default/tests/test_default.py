f38751dd5677e4fff74f43bb42ce95ac538b49de
Christopher Torgalson <manager@bedlamhotel.com>
2018-10-16 19:43:17 +0200

    Further parametrizes and expands test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,10 +8,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.mark.parametrize("version", [
-    "v1.3.0",
+@pytest.mark.parametrize('command,version', [
+    ('ddev version', 'v1.3.0'),
+    ('drush --version', '8.1.17'),
+    ('hostess --version', '0.3.0'),
 ])
-def test_release(host, version):
-    s = host.check_output('ddev version')
+def test_release(host, command, version):
+    s = host.check_output(command)
 
     assert version in s
